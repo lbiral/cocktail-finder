e6ba75be4f6d112e6dcd51dca3604e632af086ac
lbiral <90724150+lbiral@users.noreply.github.com>
2023-04-07 00:18:58 -0400

    Big mods

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 const search = document.getElementById('search');
-const cocktailList = document.getElementById('cocktail-list');
+const cocktailsList = document.getElementById('cocktails-list');
 
-search.addEventListener('input', () => {
-    const searchTerm = search.value.trim().toLowerCase();
-    const ingredients = searchTerm.split(',').map(ingredient => ingredient.trim());
+function createCocktailElement(cocktail) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = `cocktail-${cocktail.id}.html`;
+    a.textContent = cocktail.name;
+    li.appendChild(a);
+    return li;
+}
 
-    const matchingCocktails = cocktails.filter(cocktail => {
+function displayResults(results) {
+    cocktailsList.innerHTML = '';
+    results.forEach(cocktail => {
+        const cocktailElement = createCocktailElement(cocktail);
+        cocktailsList.appendChild(cocktailElement);
+    });
+}
+
+function searchCocktails() {
+    const searchInput = document.getElementById('search-input').value.toLowerCase().trim();
+    const ingredients = searchInput.split(',').map(ingredient => ingredient.trim());
+
+    const results = cocktails.filter(cocktail => {
         return ingredients.every(ingredient => cocktail.ingredients.includes(ingredient));
     });
 
-    cocktailList.innerHTML = '';
-    for (const cocktail of matchingCocktails) {
-        const listItem = document.createElement('li');
-        listItem.textContent = cocktail.name;
-        listItem.addEventListener('click', () => {
-            window.location.href = `cocktail-${cocktail.id}.html`;
-        });
-        cocktailList.appendChild(listItem);
-    }
-});
+    displayResults(results);
+}
+
+// Event listener for search button
+document.getElementById('search-button').addEventListener('click', searchCocktails);
+
+// Display all cocktails by default
+displayResults(cocktails);
+
